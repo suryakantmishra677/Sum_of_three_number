0f1eda3c30e52e281acc6ac48759a3b6634a6fd4
suryakantmishra677 <suryakantmishra677@gmail.com>
2022-04-20 13:46:20 +0530

    commit

---
+++ src/com/company/Sum.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Sum {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 15;
+        int c = 20;
+        int sum = a + b+c;
+        System.out.println(sum);
+    }
+}
---
+++ src/practice.java
@@ -0,0 +1,6 @@
+public class practice {
+    public static void main(String[] args) {
+        System.out.println("I am starting java coding ");
+        System.out.println("I want to upload this code on github :)");
+    }
+}
